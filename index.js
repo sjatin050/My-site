013181d8e8ad57669544b72450912987586b303e
sjatin050 <63118687+sjatin050@users.noreply.github.com>
2020-05-25 23:20:02 +0530

    javascript

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-$(".mountains").on("mouseover",function(){
+
   $(".top-cloud ").fadeToggle(1000);
   $(".bottom-cloud ").fadeToggle(1000);
-});
 
-$(".mountains").on("mouseover",function()
-{
+
+
   $("h1").slideUp(2500).slideDown(2500);
   $(".heading-down").slideUp(2500).slideDown(2500);
-});
+
